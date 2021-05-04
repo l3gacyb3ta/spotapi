66a2dbd8699a68b8ba4f3e02388db125f576b048
L3gacy B3ta <l3gacy.b3ta@gmail.com>
2021-05-04 14:29:40 +0000

    Eyyy It's working!

---
+++ app.py
@@ -0,0 +1,60 @@
+import spotipy
+import json
+import flask
+from spotipy.oauth2 import SpotifyOAuth
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='513f41055fe74def8c2a9236458d8dcb',
+                                               client_secret='5c37d1a354a743ed97dcb8b797b86cb0',
+                                               redirect_uri='http://example.com/',
+                                               scope='user-read-playback-state'))
+app = flask.Flask(__name__)
+api = Api(app)
+CORS(app)
+
+class Song(Resource):
+  def __init__(self):
+    super().__init__()
+    results = sp.current_user_playing_track()
+
+    url = results['item']['album']['images'][1]['url']
+    name = results['item']['name']
+    artist = results['item']['album']['artists'][0]['name']
+    title = name + ' - ' + artist
+
+    jsondata = json.dumps({'title': title, 'image': url})
+    resp = flask.Response(response=jsondata,
+              status=200,
+              mimetype="application/json")
+    self.latest = resp
+
+  def get(self):
+      try:
+        results = sp.current_user_playing_track()
+
+        url = results['item']['album']['images'][1]['url']
+        name = results['item']['name']
+        artist = results['item']['album']['artists'][0]['name']
+        title = name + ' - ' + artist
+
+        jsondata = json.dumps({'title': title, 'image': url})
+        resp = flask.Response(response=jsondata,
+                  status=200,
+                  mimetype="application/json")
+        self.latest = resp
+        return (resp)
+      except:
+        return self.latest
+
+api.add_resource(Song, '/')
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ run.sh
@@ -0,0 +1,2 @@
+pip install -r requirements.txt
+flask run --host=0.0.0.0
